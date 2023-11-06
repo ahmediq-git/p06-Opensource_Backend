33ba0a95025bbf1cbb8772b38b35900cc193328d
Zossima-F <95336337+Zossima-F@users.noreply.github.com>
2023-11-06 15:20:42 +0500

    SDK initialization

--- Prototype/sdk/crud/crud.js
+++ Prototype/sdk/crud/crud.js
@@ -1,47 +1,47 @@
 class Crud {
-    constructor(){
+    constructor() {
         console.log("Crud class");
     }
 
-    createCollection(collectionName){
+    createCollection(collectionName) {
         //creates a collection with the given name
     }
 
-    deleteCollection(collectionId){
+    deleteCollection(collectionId) {
         //deletes a collection with the given Id
     }
 
-    addRecord(collectionName, record){
+    addRecord(collectionName, record) { // Faraz
         //adds record to the given collection
     }
 
-    deleteRecord(collectionName, recordId){
-        //deletes record from the given collection
+    deleteRecord(collectionName, recordId) { // Faraz
+        //deletes entire record from the given collection
     }
 
-    updateRecord(collectionName, recordId, newRecord){
-        //updates record in the given collection
+    updateRecord(collectionName, recordId, newRecord) { // Faraz
+        //updates fields record in the given collection
         // all the fields in the record will be updated with fields of current record
     }
 
-    replaceRecord(collectionName, recordId, newRecord){
-        //replaces the current record with the new record in the given collection
+    replaceRecord(collectionName, recordId, newRecord) { // Faraz
+        //replaces the entire current record with the new record in the given collection
     }
 
-    getRecord(collectionName, recordId){
+    getRecord(collectionName, recordId) {
         //returns the record from the given collection
     }
 
-    getCollectionId(collectionName){
+    getCollectionId(collectionName) {
         //returns the collection with the given name
     }
 
 
-    getAllRecords(collectionName){
+    getAllRecords(collectionName) {
         //returns all the records from the given collection
     }
 
-    find(collectionName, query, matches=-1){
+    find(collectionName, query, matches = -1) { // Faraz
         //returns all the records from the given collection
         //query is the search criteria, matches is the number of records to be returned
         // if matches is -1, all records are returned, else the exact number of matches of records is returned
@@ -49,10 +49,10 @@ class Crud {
         // it returns 2 objects. The first is the object containing all records,
         // the second is the object containing all record Ids
     }
-
-
-
-
 }
 
 export default Crud;
+
+// Basic Operations:
+// 1) Validate
+// 2) JSON packaging
