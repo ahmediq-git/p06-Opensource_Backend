97e79035ed288551971f735b48cb2022755cbd36
MuhammadSaad0 <muhammadsaadz100@gmail.com>
2024-02-24 22:09:14 +0500

    feat: add file upload

--- Development/Sprint-2/server/src/index.ts
+++ Development/Sprint-2/server/src/index.ts
@@ -2,9 +2,11 @@ import { Context, Hono } from "hono";
 import record_crud from "@routers/record-crud";
 import collection_crud from "@routers/collection-crud";
 import auth from "@routers/auth";
+import files from "@routers/files"
 import { cors } from "hono/cors";
 import { logConsole } from "./middleware/log-console";
 import { Initialize } from "./core/init";
+import { serveStatic } from "hono/bun"
 
 (async () => {
 	await Initialize(); //initialize all the system defined parameters and collections
@@ -14,14 +16,24 @@ const app = new Hono().basePath("/api");
 
 app.use("*", logConsole);
 app.use("*", cors());
-
 app.get("/", async (c: Context) => {
 	return c.text("Hello world");
 });
-
+app.use(
+	"/static/*",
+	serveStatic({
+	  path: "../files",
+	  onNotFound: (path, c) => {
+		console.log(`${path} is not found, you access ${c.req.path}`);
+	  },
+	})
+  );
 app.route("/record", record_crud);
 app.route("/collections", collection_crud);
 app.route("/auth", auth);
+app.route("/files", files)
+
+
 
 export default {
 	port: 3690,
---
+++ Development/Sprint-2/server/src/routers/files.ts
@@ -0,0 +1,47 @@
+
+import { FileStore } from "@tus/file-store";
+import { Server } from "@tus/server";
+import { Context, Hono } from "hono";
+import { resolve } from 'path'
+const tusServer = new Server({
+    path: '/files',
+    datastore: new FileStore({ directory: './files'})
+  });
+
+const files = new Hono();
+
+files.post("", async (c) => {
+    const data = await c.req.formData();
+    const file_x = data.get("file");
+    if(!file_x) return c.json({error: true, data: null})
+    let bytes_written = await Bun.write(`./files/${file_x.name}`, file_x)
+    if(bytes_written > 0){
+        let path = resolve(`./files/${file_x.name}`)
+        return c.json({
+            error: false,
+            data: path
+        })
+    }
+    return c.json({
+        error: true,
+        data: null
+    })
+   
+});
+
+files.get("/get_file", async (c: Context) => {
+    const params = c.req.query();
+	const { file_name } = params as {
+        file_name: string;
+	};
+    if(file_name){
+    let path = resolve(`./files/${file_name}`)
+    return new Response(Bun.file(path))
+    }
+    return c.json({
+        error: true,
+        data: null
+    })
+})
+
+export default files
