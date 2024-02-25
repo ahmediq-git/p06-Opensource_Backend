024bc4f56f708d668a3e7dd5ba18841bb6080bdc
lazersocks <moizwwww@gmail.com>
2024-02-26 02:39:58 +0500

    Settings page

--- Development/Sprint-2/frontend/src/App.jsx
+++ Development/Sprint-2/frontend/src/App.jsx
@@ -3,8 +3,8 @@ import HomePage from "./pages/Homepage";
 import Logs from "./pages/Logs";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Settings from "./pages/Settings";
 import ProtectedRoutes from "./pages/ProtectedRoutes";
-import Logs from "./components/Logs";
 
 function App() {
 	return (
@@ -13,6 +13,7 @@ function App() {
 				{/* <Route path="/" element={<ProtectedRoutes />}> */}
 				<Route path="/" element={<HomePage />}></Route>
 				<Route path="/logs" element={<Logs />}></Route>
+				<Route path="/settings" element={<Settings />}></Route>
 					{/* <Route path="/login" element={<Login />}></Route>
 					<Route path="/init" element={<Signup />}></Route> */}
 				{/* </Route> */}
---
+++ Development/Sprint-2/frontend/src/components/FileSettings.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function FileSettings() {
+  return (
+    <div>FileSettings</div>
+  )
+}
