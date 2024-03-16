807798d93b4d8eef0404354898e665ecb0909273
MuhammadSaad0 <muhammadsaadz100@gmail.com>
2024-03-17 03:18:10 +0500

    fix: reduce schema generation time 10x

--- Development/Sprint-2/server/src/routers/schema.ts
+++ Development/Sprint-2/server/src/routers/schema.ts
@@ -6,72 +6,77 @@ import { listRecords } from "@src/controllers/record-crud";
 const schema = new Hono();
 
 schema.get('/get_schema', async (c) => {
+    try {
+        const all_collections = await getAllCollections();
+        let schemaPromises = all_collections.map(async (current_col) => {
+            let schema:any = {
+                'concrete': [],
+                'loose': []
+            };
+            let all_records: any = await listRecords({}, {
+                'sort': { ['createdAt']: 0 },
+                limit: 0,
+                offset: 0
+            }, current_col);
+            let keyInfo:any = {};
+            let foreignKeyInfo:any = {};
 
-    const all_collections = await getAllCollections();
-    let schemas = [];
+            for (let j = 0; j < all_records.length; j++) {
+                const record = all_records[j];
+                for (let key in record) {
+                    if (record[key] != null && record[key] != undefined && typeof record[key] === 'object' && 'type' in record[key] && record[key]['type'] == 'foreign_key') {
+                        let ref = record[key]['collection'];
+                        if (ref == undefined || ref == null) {
+                            continue;
+                        }
+                        let key_with_ref = `${key} ---> ${ref}`
+                        if (foreignKeyInfo[key_with_ref]) {
+                            foreignKeyInfo[key_with_ref].count++;
+                        } else {
+                            foreignKeyInfo[key_with_ref] = { count: 1, ref };
+                        }
+                    } else {
+                        let type = typeof key;
+                        let key_with_type = `${key} : ${type}`
+                        if (keyInfo[key_with_type]) {
+                            keyInfo[key_with_type].count++;
+                        } else {
+                            keyInfo[key_with_type] = { count: 1, type };
+                        }
+                    }
 
-    for (let i = 0; i < all_collections.length; i++) {
-        let schema: any = {
-            'concrete': [],
-            'loose': []
-        };
-        let current_col = all_collections[i];
-        let all_records: any = await listRecords({}, {
-            'sort': { ['createdAt']: 0 },
-            limit: 0,
-            offset: 0
-        }, current_col);
-        let keyInfo: any = {};
-        let foreignKeyInfo: any = {};
-        for (let j = 0; j < all_records.length; j++) {
-            const record = all_records[j];
-            for (let key in record) {
-                if (record[key] != null && record[key] != undefined && typeof record[key] === 'object' && 'type' in record[key] && record[key]['type'] == 'foreign_key') {
-                    let ref = record[key]['collection'];
-                    if(ref == undefined || ref == null ){
-                        continue
-                    }
-                    let key_with_ref = `${key} ---> ${ref}`
-                    if (foreignKeyInfo[key_with_ref]) {
-                        foreignKeyInfo[key_with_ref].count++;
-                    } else {
-                        foreignKeyInfo[key_with_ref] = { count: 1, ref };
-                    }
-                } else {
-                    let type = typeof key;
-                    let key_with_type = `${key} : ${type}`
-                    if (keyInfo[key_with_type]) {
-                        keyInfo[key_with_type].count++;
-                    } else {
-                        keyInfo[key_with_type] = { count: 1, type };
-                    }
                 }
 
             }
+            for (let key in keyInfo) {
+                if (keyInfo[key].count == all_records.length) {
+                    schema['concrete'].push(key);
+                } else {
+                    schema['loose'].push(key)
+                }
+            }
+            for (let key in foreignKeyInfo) {
+                if (foreignKeyInfo[key].count == all_records.length) {
+                    schema['concrete'].push(key);
+                } else {
+                    schema['loose'].push(key)
+                }
+            }
+            return [current_col, schema];
+        });
 
-        }
-        for (let key in keyInfo) {
-            if (keyInfo[key].count == all_records.length) {
-                schema['concrete'].push(key);
-            } else {
-                schema['loose'].push(key)
-            }
-        }
-        for (let key in foreignKeyInfo) {
-            if (foreignKeyInfo[key].count == all_records.length) {
-                schema['concrete'].push(key);
-            } else {
-                schema['loose'].push(key)
-            }
-        }
-        schemas.push([current_col, schema]);
+        let schemas = await Promise.all(schemaPromises);
+        return c.json({
+            data: schemas,
+            error: null
+        });
+    } catch (error) {
+        return c.json({
+            data: null,
+            error: 'Error generating schema'
+        });
     }
-    return c.json({
-        data: schemas,
-        error: null
-    });
 })
-
 function generateMarkdownTable(schemas: any[][]) {
     let markdown = "";
     schemas.forEach(([schemaName, schemaData]) => {
