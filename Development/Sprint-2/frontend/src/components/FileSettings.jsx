eef63712a805ea8ddede796fc1cf924060ed50a6
Zossima-F <95336337+Zossima-F@users.noreply.github.com>
2024-03-18 13:04:02 +0500

    fixed bugs with FileSettings

--- Development/Sprint-2/frontend/src/components/FileSettings.jsx
+++ Development/Sprint-2/frontend/src/components/FileSettings.jsx
@@ -108,9 +108,9 @@ export default function FileSettings() {
               <input
                 type="checkbox"
                 className="sr-only peer"
-                // setData and handleSwitchToggle are used to handle the switch state
                 onChange={(e) => setData({ ...data, useBlobStorage: e.target.checked })}
-                checked={data.useBlobStorage} // Set the switch state based on isSwitchOn state
+                onInput={updateFileSettings}
+                checked={data.useBlobStorage}
               />
               <div className={`relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600`} />
             </label>
